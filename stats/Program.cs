b5a84887883c67a2cbe1f187884a3818dc41bed0
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-07 11:04:27 -0700

    Changed to use OOP

--- stats/Program.cs
+++ stats/Program.cs
@@ -4,4 +4,7 @@ using System.IO;
 Console.WriteLine("Welcome To this File Statistics Displayer!"); 
 Console.WriteLine("Please enter the directory of your file below");
 string path = Console.ReadLine(); 
-string[] lines = File.ReadAllLines(path);
+int lineCount = 0; 
+pathData p = new pathData(path); 
+lineCount = p.getLineCount(); 
+
