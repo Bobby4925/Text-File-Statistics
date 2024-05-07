aa262b4bc42d992a1202c13e81facdc854d020d0
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-06 23:52:15 -0700

    Inital Commit

---
+++ stats/Program.cs
@@ -0,0 +1,7 @@
+﻿using System; 
+using System.IO; 
+
+Console.WriteLine("Welcome To this File Statistics Displayer!"); 
+Console.WriteLine("Please enter the directory of your file below");
+string path = Console.ReadLine(); 
+string[] lines = File.ReadAllLines(path);
