6bcede2046f41a017b9632e6282f5ac7032319e3
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-11 23:49:22 -0700

    Fixed The Word Count and Other Issues

--- stats/Program.cs
+++ stats/Program.cs
@@ -3,8 +3,12 @@ using System.IO;
 
 Console.WriteLine("Welcome To this File Statistics Displayer!"); 
 Console.WriteLine("Please enter the directory of your file below");
-string path = Console.ReadLine(); 
+//string path = Console.ReadLine(); 
+string path = "/Users/ayushb/Desktop/Text File Stats/stats/test.txt";
 int lineCount = 0; 
 pathData p = new pathData(path); 
 lineCount = p.getLineCount(); 
+Console.WriteLine(lineCount);
+Console.WriteLine(p.getWordCount());
+Console.WriteLine(p.getCharacterCount());
 
