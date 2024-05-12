d20fc58873e8089a497068c01b0ec6158d58fba4
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-12 00:06:53 -0700

    Uh

--- stats/Program.cs
+++ stats/Program.cs
@@ -3,8 +3,7 @@ using System.IO;
 
 Console.WriteLine("Welcome To this File Statistics Displayer!"); 
 Console.WriteLine("Please enter the directory of your file below");
-//string path = Console.ReadLine(); 
-string path = "/Users/ayushb/Desktop/Text File Stats/stats/test.txt";
+string path = Console.ReadLine(); 
 int lineCount = 0; 
 int wordCount = 0;
 int charCount = 0; 
