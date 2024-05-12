e65f0cdeb6ca6c70213bd5e5029a3d3930642685
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-12 00:03:59 -0700

    Uhhh Is it Fixed?

--- stats/Program.cs
+++ stats/Program.cs
@@ -10,15 +10,9 @@ int wordCount = 0;
 int charCount = 0; 
 pathData p = new pathData(path); 
 lineCount = p.getLineCount(); 
-<<<<<<< HEAD
 wordCount = p.getWordCount();
 charCount = p.getCharacterCount();
 Console.WriteLine(lineCount);
 Console.WriteLine(wordCount);
 Console.WriteLine(charCount);
-=======
-Console.WriteLine(lineCount);
-Console.WriteLine(p.getWordCount());
-Console.WriteLine(p.getCharacterCount());
->>>>>>> 6bcede2046f41a017b9632e6282f5ac7032319e3
 
