36217d7977cbe0704f05a99c7f2b7476f385ac1f
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-13 11:10:22 -0700

    Added Unique Word Count and Display of Counts

--- stats/Program.cs
+++ stats/Program.cs
@@ -1,12 +1,10 @@
-﻿using System; 
-using System.IO; 
-
-Console.WriteLine("Welcome To this File Statistics Displayer!"); 
+﻿Console.WriteLine("Welcome To this File Statistics Displayer!"); 
 Console.WriteLine("Please enter the directory of your file below");
 string path = Console.ReadLine(); 
 int lineCount = 0; 
 int wordCount = 0;
-int charCount = 0; 
+int charCount = 0;
+int uniqueCount = 0;  
 pathData p = new pathData(path); 
 //Line Count
 lineCount = p.getLineCount();
@@ -14,7 +12,9 @@ lineCount = p.getLineCount();
 wordCount = p.getWordCount();
 //Character Count
 charCount = p.getCharacterCount();
-Console.WriteLine(lineCount);
-Console.WriteLine(wordCount);
-Console.WriteLine(charCount);
-
+//Unique Word Count 
+uniqueCount = p.uniqueWordCount();
+Console.WriteLine($"Current Line Count: {lineCount}");
+Console.WriteLine($"Current Word Count: {wordCount}");
+Console.WriteLine($"Current Character Count: {charCount}");
+Console.WriteLine($"Current Unique Word Count: {uniqueCount}"); 
